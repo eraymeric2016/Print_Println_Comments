7f02b8983a6567767a3db6174130acf5ffd3d2f9
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:08:45 +0300

    Add files via upload

---
+++ Print_Println_Comments.java
@@ -0,0 +1,16 @@
+package week_03;
+
+public class Print_Println_Comments {
+
+    public static void main(String[] args) {
+
+        System.out.println("Adam");
+        System.out.print("Eyup\n");
+        System.out.println("\t eray");
+
+
+    }
+
+
+
+}
